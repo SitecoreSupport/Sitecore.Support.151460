27cf7be76042d30edfb4c5d8a248e3e43e487d0d
sc-spl <spl@sitecore.net>
2017-03-09 19:51:31 +0200

    Initial commit

---
+++ src/Sitecore.Support.151460/FXM/Pipelines/Tracking/BeforeEvent/EnsureCurrentPageIsTrackedProcessor.cs
@@ -0,0 +1,53 @@
+﻿using Sitecore.Analytics.Tracking;
+using Sitecore.Diagnostics;
+using Sitecore.FXM.Pipelines.Tracking;
+using Sitecore.FXM.Tracking;
+
+namespace Sitecore.Support.FXM.Pipelines.Tracking.BeforeEvent
+{
+    public class EnsureCurrentPageIsTrackedProcessor : Sitecore.FXM.Pipelines.Tracking.BeforeEvent.EnsureCurrentPageIsTrackedProcessor
+    {
+
+        public new void Process(ITrackingArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            Assert.ArgumentNotNull(args.TrackerProvider, "TrackerProvider");
+            Assert.ArgumentNotNull(args.TrackerProvider.Current, "Current tracker provider");
+            Assert.ArgumentNotNull(args.TrackingRequest, "TrackingRequest");
+            if (args.CurrentPageVisit == null)
+            {
+                if (!args.TrackerProvider.Current.IsActive && (args.TrackerProvider.Current.Interaction == null))
+                {
+                    args.TrackerProvider.Current.StartTracking();
+                }
+
+                //patch start
+                if (!args.TrackerProvider.Current.IsActive)
+                {
+                    args.TrackerProvider.Current.Interaction.ValueAggregatedEvent+=
+                         delegate (int value) {
+                             if (args.TrackerProvider.Current.Session.Contact != null)
+                             {
+                                 IContactSystemInfoContext system = args.TrackerProvider.Current.Contact.System;
+                                 system.Value += value;
+                             }
+                         };
+                }
+                //patch end
+
+                if (args.TrackerProvider.Current.Interaction != null)
+                {
+                    IPageContext currentPageInInteraction = this.GetCurrentPageInInteraction(args.TrackerProvider.Current.Interaction, args.TrackingRequest.Url);
+                    if (currentPageInInteraction != null)
+                    {
+                        args.CurrentPageVisit = currentPageInInteraction;
+                    }
+                }
+                if (args.CurrentPageVisit == null)
+                {
+                    args.AbortAndFailPipeline("the current page has not been tracked in the current session.", TrackingResultCode.CurrentPageMustBeTracked);
+                }
+            }
+        }
+    }
+}
